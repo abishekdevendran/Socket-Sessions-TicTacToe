fa00b53f14a3885c9aa76a3f9605f2467a624cac
Abishek Devendran <abishekdevendran@gmail.com>
2022-06-13 17:46:27 +0530

    typo change

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname + "/frontend/public/index.html"));
 });
 io.use(wrap(sessionMiddleware));
 
