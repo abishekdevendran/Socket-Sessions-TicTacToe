0417548bb02a54eaa0bbd77930b38ea18ab35c52
Abishek Devendran <abishekdevendran@gmail.com>
2022-06-15 15:44:50 +0530

    #2 syntax error

--- helpers/redisStore.js
+++ helpers/redisStore.js
@@ -3,7 +3,7 @@ const SECRET = process.env.SECRET || "secret";
 const session = require("express-session");
 
 const { createClient } = require("redis");
-const redisClient = createClient({ legacyMode: true, process.env.NODE_ENV==="production"&&process.env.REDIS_URL });
+const redisClient = createClient({ legacyMode: true, process.env.NODE_ENV==="production"&&url:process.env.REDIS_URL });
 redisClient.connect().then(console.log("redis connected successfully")).catch(console.error("redis connection failed"));
 const RedisStore = require("connect-redis")(session);
 const sessionMiddleware = session({
